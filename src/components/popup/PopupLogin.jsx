1aab42d40991b55e64b1f38e69600d7a46b348db
Hiboo93 <99762397+Hiboo93@users.noreply.github.com>
2024-03-03 23:39:21 +0100

    ✨F09: creating feature handleSubmit / 📱 F09: Responsive improvement / and end of project

--- src/components/popup/PopupLogin.jsx
+++ src/components/popup/PopupLogin.jsx
@@ -4,12 +4,27 @@ import { FaFacebook } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 
 const PopupLogin = ({ showPopup, setShowPopup }) => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    const dataForm = new FormData(form)
+
+    const email = dataForm.get("email")
+    const password = dataForm.get("password")
+
+    console.log(email, password);
+    form.reset()
+    setShowPopup(false)
+  }
+  
+
   return (
     <>
       {showPopup && (
         <div>
           <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
-            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white rounded-md duration-200 w-[300px]">
+            <div className="fixed top-[50%]  right-[20%] lg:right-[30%] xl:right-[40%]  p-4 shadow-md bg-white rounded-md duration-200 w-[300px]">
               {/* header section */}
               <div className="flex items-center justify-between">
                 <div>
@@ -25,23 +40,27 @@ const PopupLogin = ({ showPopup, setShowPopup }) => {
 
               {/* login form section */}
               <div className="mt-4">
-                <input
-                  type="email"
-                  placeholder="Enter Email"
-                  className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"
-                />
-                <input
-                  type="password"
-                  placeholder="Enter Password"
-                  className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"
-                />
-
-                {/* Login button section */}
-                <div>
-                  <button className="w-full bg-primary text-white p-2 rounded-md border-0">
-                    Login
-                  </button>
-                </div>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter Email"
+                    className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"
+                  />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter Password"
+                    className="w-full rounded-md border border-gray-300 dark:border-gray-500 px-2 py-1 mb-4"
+                  />
+                  
+                  {/* Login button section */}
+                  <div>
+                    <button type="submit" className="w-full bg-primary text-white p-2 rounded-md border-0">
+                      Login
+                    </button>
+                  </div>
+                </form>
 
                 {/* social login */}
                 <div className="mt-4">
